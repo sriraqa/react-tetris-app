282fcd379151f169f660382c8856717a4284bd75
Sarah Q <79424592+sriraqa@users.noreply.github.com>
2022-07-29 17:06:09 -0400

    added responsive grid

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { StyledStage } from './styles/StyledStage';
 
 import Cell from './Cell';
 
 const Stage = ({ stage }) => ( //prop called stage
-    <div>
+    <StyledStage width={stage[0].length} height={stage.length}>
         {/* map through stage array to get row array. Map through row array to get cell x value */}
         {/* there are no occupied cells so set to 0 */}
         {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
-    </div>
+    </StyledStage>
 )
 
 export default Stage;
