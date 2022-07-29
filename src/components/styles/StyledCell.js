7075f5bda5e6018f759892d8ddbe5ff849f4ccff
Sarah Q <79424592+sriraqa@users.noreply.github.com>
2022-07-29 16:53:23 -0400

    added background and blocks

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
 // styled component uses ``
+// $ gets props to send into styled components as an inline function
+// border shadows
+// turner operator (if type is 0, then no border, otherwise solid border)
 export const StyledCell = styled.div`
     width: auto;
-    background: rgba(${props => props.color}, 0.7);
+    background: rgba(${props => props.color}, 0.8);
+    border: ${props => (props.type === 0 ? '0px solid' : '4px solid')};
+    border-bottom-color: rgba(${props => props.color}, 0.1);
+    border-left-color: rgba(${props => props.color}, 0.3);
+    border-right-color: rgba(${props => props.color}, 1);
+    border-top-color: rgba(${props => props.color}, 1);
 `
